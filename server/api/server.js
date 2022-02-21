2e4e044a94c4fd202993ea249c3699309ef4496c
ewhite1999 <ewhite1999@gmail.com>
2022-02-21 11:10:29 +0000

    add routing to api

--- server/api/server.js
+++ server/api/server.js
@@ -5,6 +5,9 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const habitsRoutes = require("./routes/habits");
+server.use("/habits", habitsRoutes);
+
 server.get("/", (req, res) => res.send("Hello world"));
 
 module.exports = server;
