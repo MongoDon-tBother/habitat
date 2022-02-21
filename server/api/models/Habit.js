c0bb9d959880e1f465e0b3e1f8bbaf5dbc9b2b4f
ewhite1999 <ewhite1999@gmail.com>
2022-02-21 14:50:16 +0000

    add comments to habit module

--- server/api/models/Habit.js
+++ server/api/models/Habit.js
@@ -13,14 +13,17 @@ module.exports = class Habit {
   static findByUser(id) {
     return new Promise(async (resolve, reject) => {
       try {
+        // get all habit data matching that user id
         let habitData = await db.query(
           `SELECT * FROM habits WHERE user_id = $1;`,
           [id]
         );
         habitData = habitData.rows;
         for (let data of habitData) {
+          // get the subhabits that match the habit id
           const subData = await findSubhabits(data.id);
           if (subData.length) data.subhabits = subData;
+          // get the frequency that match the habit id
           const frequencyData = await findFrequency(data.frequency_id);
           if (frequencyData.length) data.frequency = frequencyData;
         }
@@ -38,6 +41,7 @@ module.exports = class Habit {
     return new Promise(async (resolve, reject) => {
       try {
         const { name, frequency, username, subhabits } = habitData;
+        // Check to see if a frequency matching the new habit frequency exists
         let duplicate = false;
         let frequency_id;
         let previousFrequency = await db.query(`SELECT id FROM frequency`);
@@ -49,6 +53,7 @@ module.exports = class Habit {
             break;
           }
         }
+        // If that frequency doesn't already exist add it to the DB
         if (!duplicate) {
           const frequencyBool = frequency.map((m) => {
             if (m === 1) {
@@ -66,19 +71,23 @@ module.exports = class Habit {
           );
           frequency_id = frequency_id.rows[0].id;
         }
+        // set complete and streak to false as its a new habit
         const complete = false;
         const streak = 0;
+        // find the user id that matches the username
         let user_id = await db.query(
           `SELECT id FROM users WHERE username = $1;`,
           [username]
         );
         user_id = user_id.rows[0].id;
+        // Store the new habit in the DB and return the id
         let habit_id = await db.query(
           `INSERT INTO habits (name, frequency_id, complete, streak, user_id)
            VALUES ($1, $2, $3, $4, $5) RETURNING id;`,
           [name, frequency_id, complete, streak, user_id]
         );
         habit_id = habit_id.rows[0].id;
+        // If there are any subhabits add them to the DB
         if (subhabits) {
           for (let subhabit of subhabits) {
             await db.query(
@@ -88,6 +97,7 @@ module.exports = class Habit {
             );
           }
         }
+        // Return all habits for the user
         resolve(this.findByUser(user_id));
       } catch (err) {
         reject(err);
