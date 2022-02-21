32114ea38c59357936ba52bf28cd0fea8518d9fc
saminakhan999 <Samina.khan9999@gmail.com>
2022-02-21 10:56:07 +0000

    hash links

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -6,13 +6,41 @@ function renderHomepage(){
   main.appendChild(logo)
   const login_book = document.createElement('login_book');
 
+}
+
 function renderLoginForm() {
   const deardiary = document.createElement("h1");
   deardiary.id = "deardiary";
   deardiary.textContent = "Dear Diary";
   main.appendChild(deardiary);
+
+  const loginform = document.createElement("form");
+  loginform.id = "loginform";
+
+  const usernamelabel = document.createElement("label");
+  usernamelabel.textContent = "Username";
+  loginform.appendChild(usernamelabel);
+  const usernameinput = document.createElement("input");
+  usernameinput.type = "text";
+  usernameinput.id = "usernameinput";
+  loginform.appendChild(usernameinput);
+
+  const passwordlabel = document.createElement("label");
+  passwordlabel.textContent = "Password";
+  loginform.appendChild(passwordlabel);
+  const passwordinput = document.createElement("input");
+  passwordinput.type = "password";
+  passwordinput.id = "passwordinput";
+  loginform.appendChild(usernameinput);
+
+  main.appendChild(loginform)
+
+  
+  /*
   const fields = [
+    { tag: "label", attributes: { type: "text", name: "username" } },
     { tag: "input", attributes: { type: "text", name: "username" } },
+    { tag: "label", attributes: { type: "text", name: "username" } },
     { tag: "input", attributes: { type: "password", name: "password" } },
     { tag: "input", attributes: { type: "submit", value: "Login" } }
   ];
@@ -26,6 +54,7 @@ function renderLoginForm() {
   });
   form.addEventListener("submit", requestLogin);
   main.appendChild(form);
+  */
 }
 
-}
+
