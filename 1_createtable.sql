18004d439589eed62ff102aca9acb6fe2dada961
saminakhan999 <Samina.khan9999@gmail.com>
2022-02-21 09:33:01 +0000

    added database

---
+++ 1_createtable.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS habits;
+DROP TABLE IF EXISTS frequency;
+DROP TABLE IF EXISTS subhabits;
+
+
+
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username varchar NOT NULL,
+    email varchar NOT NULL,
+  password_digest varchar NOT NULL
+);
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    name varchar NOT NULL,
+    frequency_id int NOT NULL,
+    complete boolean NOT NULL,
+    streak int NOT NULL,
+    user_id int NOT NULL
+)           
+
+CREATE TABLE habits (
+  id serial PRIMARY KEY,
+  name varchar NOT NULL,
+  frequency_id int NOT NULL,
+  complete boolean NOT NULL,
+  streak int NOT NULL,
+  user_id int NOT NULL
+)
+CREATE TABLE frequency (
+  id serial PRIMARY KEY,
+  monday boolean NOT NULL,
+  tuesday boolean NOT NULL,
+  wednesday boolean NOT NULL,
+  thursday boolean NOT NULL,
+  friday boolean NOT NULL,
+  saturday boolean NOT NULL,
+  sunday boolean NOT NULL
+)
+
+CREATE TABLE subhabits (
+  id serial PRIMARY KEY,
+  name varchar NOT NULL,
+  complete boolean NOT NULL,
+  habbit_id int NOT NULL
+)
