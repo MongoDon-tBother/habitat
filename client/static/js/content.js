b0d52ad622827c1524d1517b3306d3ceed6338f0
saminakhan999 <Samina.khan9999@gmail.com>
2022-02-21 10:20:15 +0000

    links in html

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -1,1 +1,27 @@
-k
+function renderLoginForm() {
+  const fields = [
+    {
+      tag: "input",
+      attributes: { type: "email", name: "email", placeholder: "Email" }
+    },
+    {
+      tag: "input",
+      attributes: {
+        type: "password",
+        name: "password",
+        placeholder: "Password"
+      }
+    },
+    { tag: "input", attributes: { type: "submit", value: "Login" } }
+  ];
+  const form = document.createElement("form");
+  fields.forEach((f) => {
+    let field = document.createElement(f.tag);
+    Object.entries(f.attributes).forEach(([a, v]) => {
+      field.setAttribute(a, v);
+      form.appendChild(field);
+    });
+  });
+  form.addEventListener("submit", requestLogin);
+  main.appendChild(form);
+}
