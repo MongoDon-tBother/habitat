f1068a4b15b83e5a4371204490324dfd6193d737
ewhite1999 <ewhite1999@gmail.com>
2022-02-21 15:25:01 +0000

    add put/patch route

--- server/api/routes/habits.js
+++ server/api/routes/habits.js
@@ -5,5 +5,7 @@ const habitsController = require("../controllers/habits");
 router.get("/:user_id", habitsController.show);
 router.post("/", habitsController.create);
 router.delete("/:habit_id", habitsController.destroy);
+router.put("/:habit_id", habitsController.update);
+router.patch("/:habit_id", habitsController.update);
 
 module.exports = router;
