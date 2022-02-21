a93a6fb7eba61299c011f7798ba8eecd003a4daf
ewhite1999 <ewhite1999@gmail.com>
2022-02-21 16:31:02 +0000

    add update controller

--- server/api/controllers/habits.js
+++ server/api/controllers/habits.js
@@ -20,11 +20,21 @@ async function create(req, res) {
 
 async function destroy(req, res) {
   try {
-    const habit = await Habit.destroyHabit(req.params.habit_id);
+    await Habit.destroyHabit(req.params.habit_id);
     res.status(204).end();
   } catch (err) {
     res.status(404).json({ err });
   }
 }
 
-module.exports = { show, create, destroy };
+async function update(req, res) {
+  try {
+    const habit = await Habit.findByHabit(req.params.habit_id);
+    habit.updateHabit(req.body);
+    res.status(204).end();
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+}
+
+module.exports = { show, create, destroy, update };
