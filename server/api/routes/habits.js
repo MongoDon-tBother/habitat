f89d3924206f9e7f2a90c7de72585e638536d302
ewhite1999 <ewhite1999@gmail.com>
2022-02-22 14:35:31 +0000

    get by habit id route

--- server/api/routes/habits.js
+++ server/api/routes/habits.js
@@ -7,6 +7,6 @@ router.post("/", habitsController.create);
 router.delete("/:habit_id", habitsController.destroy);
 router.put("/:habit_id", habitsController.update);
 router.patch("/:habit_id", habitsController.update);
+router.get("/hab_id/:habit_id", habitsController.showHab);
 
 module.exports = router;
-
