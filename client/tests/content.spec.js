2313e8b01aa9929ba0d73313300ddf445f70fded
jalexxx <jumaanahalex@gmail.com>
2022-02-22 21:51:51 +0000

    written the login and sign up form tests

--- client/tests/content.spec.js
+++ client/tests/content.spec.js
@@ -24,7 +24,7 @@ describe("the homepage", () => {
     }
   });
 
-  describe("first book - login", () => {
+  describe("first book ", () => {
     test("there is a book for the login"), () => {
       const login_book = document.querySelector("#login_book");
       expect (login_book).toBe("login_book")
@@ -32,6 +32,54 @@ describe("the homepage", () => {
     }
   });
 
+  describe("second book", () => {
+    test("there is a second book for the sign up"), () => {
+      const signup_book = document.querySelector("#signup_book");
+      expect (signup_book).toBe("signup_book")
+      
+    }
+  });
+
+  describe("shelf", () => {
+    test("there will be a shelf "), () => {
+      const shelf = document.querySelector("#shelf");
+      expect (shelf).toBe("shelf")
+      
+    }
+  });
   
-  
+});
+
+describe("login form", () => {
+  describe("fields", () => {
+    test("there is the content within the fields"), () => {
+      const form = document.querySelector("form");
+      expect(form).toBeTruthy();
+    }
+  });
+
+  describe(" login submit button", () => {
+    test("there is a submit button for the login "), () => {
+      const form = document.querySelector("form");
+      expect(onSubmit).toHaveBeenCalled();
+    }
+  });
+
+});
+
+describe("signup form", () => {
+  describe("fields", () => {
+    test("there is the content within the fields"), () => {
+      const form = document.querySelector("form");
+      expect(form).toBeTruthy();
+    }
+  });
+
+  describe("signup submit button", () => {
+    test("there is a submit button for the signup "), () => {
+      const form = document.querySelector("form");
+      expect(onSubmit).toHaveBeenCalled();
+    }
+  });
+
 });
