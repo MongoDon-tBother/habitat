2e0773a62558e0afa3a409e560967c2eda1b85b2
ewhite1999 <ewhite1999@gmail.com>
2022-02-23 12:14:31 +0000

    add xbtn to subhabits

--- client/static/js/handler_helpers.js
+++ client/static/js/handler_helpers.js
@@ -1,32 +1,32 @@
-/**
- * @param  {arr} subhabits - the array of objects containing the subhabits
- */
-const displaySubhabits = (subhabits, frequency) => {
-  if (!subhabits) return "";
+// /**
+//  * @param  {arr} subhabits - the array of objects containing the subhabits
+//  */
+// const displaySubhabits = (subhabits, frequency) => {
+//   if (!subhabits) return "";
 
-  const subList = document.createElement("ul");
-  subList.classList.add("sub_list");
-  subhabits.forEach((subhabit) => {
-    const name = subhabit.name;
-    const listItem = document.createElement("li");
-    listItem.classList.add("sub_task");
-    listItem.innerText = name;
-    subList.appendChild(listItem);
+//   const subList = document.createElement("ul");
+//   subList.classList.add("sub_list");
+//   subhabits.forEach((subhabit) => {
+//     const name = subhabit.name;
+//     const listItem = document.createElement("li");
+//     listItem.classList.add("sub_task");
+//     listItem.innerText = name;
+//     subList.appendChild(listItem);
 
-    const completed = subhabit.complete;
-    // check if need to be completed today and hasn't been completed
-    if (dateCheck(completed, frequency)) {
-      // if so mark show solid
-      listItem.classList.add("sub_incomplete");
-      listItem.classList.remove("sub_done");
-    } else {
-      //  if not grey out
-      listItem.classList.remove("sub_incomplete");
-      listItem.classList.add("sub_done");
-    }
-  });
-  return subList;
-};
+//     const completed = subhabit.complete;
+//     // check if need to be completed today and hasn't been completed
+//     if (dateCheck(completed, frequency)) {
+//       // if so mark show solid
+//       listItem.classList.add("sub_incomplete");
+//       listItem.classList.remove("sub_done");
+//     } else {
+//       //  if not grey out
+//       listItem.classList.remove("sub_incomplete");
+//       listItem.classList.add("sub_done");
+//     }
+//   });
+//   return subList;
+// };
 /**
  * @param  {string} doneDate - The timestamp stored in the habit
  * @param  {arr} frequency - The array of frequency stored in the habit
@@ -45,7 +45,10 @@ const dateCheck = (doneDate, frequency) => {
     today.toString() !== lastCompleted.toString()
   );
 };
-
+/**
+ * Generates the checkboxes to select frequency
+ * @param  {arr} frequency - An array of 1s and 0s to say which day it is repeated on
+ */
 const createFrequencySelect = (frequency) => {
   const returnArr = [];
 
@@ -64,17 +67,42 @@ const createFrequencySelect = (frequency) => {
   return returnArr;
 };
 
+function renderSubHabitInput(subhabit) {
+  const subWrap = document.createElement("div");
+  subWrap.classList.add("sub_wrap");
+
+  const newHabitForm = document.getElementById("newHabitForm");
+  const subHabitName = document.createElement("input");
+  subHabitName.name = "subHabitName";
+  subHabitName.classList.add("subHabitName");
+  console.log(subhabit);
+  if (subhabit.name) {
+    subHabitName.value = subhabit.name;
+  } else {
+    subHabitName.placeholder = "Subhabit Name";
+  }
+  subWrap.appendChild(subHabitName);
+
+  const x_btn = document.createElement("button");
+  x_btn.innerText = "x";
+  x_btn.classList.add("rm_subhabit");
+  subWrap.appendChild(x_btn);
+
+  newHabitForm.appendChild(subWrap);
+}
+
 const createBtn = () => {
-  const btn = document.createElement("button");
+  const btn = document.createElement("input");
   btn.classList.add("btn", "update_btn");
   btn.innerText = "Update";
+  btn.type = "submit";
 
   return btn;
 };
 
 module.exports = {
-  displaySubhabits,
   createBtn,
   dateCheck,
-  createFrequencySelect
+  createFrequencySelect,
+  renderSubHabitInput
 };
