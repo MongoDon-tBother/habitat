b906bb38a149db729c83028842b1ab6936ed761f
ewhite1999 <ewhite1999@gmail.com>
2022-02-23 12:57:30 +0000

    remove console.logs

--- client/static/js/handler_helpers.js
+++ client/static/js/handler_helpers.js
@@ -1,36 +1,3 @@
-// /**
-//  * @param  {arr} subhabits - the array of objects containing the subhabits
-//  */
-// const displaySubhabits = (subhabits, frequency) => {
-//   if (!subhabits) return "";
-
-//   const subList = document.createElement("ul");
-//   subList.classList.add("sub_list");
-//   subhabits.forEach((subhabit) => {
-//     const name = subhabit.name;
-//     const listItem = document.createElement("li");
-//     listItem.classList.add("sub_task");
-//     listItem.innerText = name;
-//     subList.appendChild(listItem);
-
-//     const completed = subhabit.complete;
-//     // check if need to be completed today and hasn't been completed
-//     if (dateCheck(completed, frequency)) {
-//       // if so mark show solid
-//       listItem.classList.add("sub_incomplete");
-//       listItem.classList.remove("sub_done");
-//     } else {
-//       //  if not grey out
-//       listItem.classList.remove("sub_incomplete");
-//       listItem.classList.add("sub_done");
-//     }
-//   });
-//   return subList;
-// };
-/**
- * @param  {string} doneDate - The timestamp stored in the habit
- * @param  {arr} frequency - The array of frequency stored in the habit
- */
 const dateCheck = (doneDate, frequency) => {
   const today = new Date().toString().split(" ").slice(0, 4);
   const todayDay = today[0];
@@ -75,7 +42,6 @@ function renderSubHabitInput(subhabit) {
   const subHabitName = document.createElement("input");
   subHabitName.name = "subHabitName";
   subHabitName.classList.add("subHabitName");
-  console.log(subhabit);
   if (subhabit.name) {
     subHabitName.value = subhabit.name;
   } else {
@@ -94,8 +60,8 @@ function renderSubHabitInput(subhabit) {
 const createBtn = () => {
   const btn = document.createElement("input");
   btn.classList.add("btn", "update_btn");
-  btn.innerText = "Update";
   btn.type = "submit";
+  btn.value = "Update";
 
   return btn;
 };
