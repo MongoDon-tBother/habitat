4cdef41a3e0e0f74edf3cbcc887fac5ad4e9f618
saminakhan999 <samina.khan9999@gmail.com>
2022-02-24 18:02:16 +0000

    added await initdb

--- server/api/test/server.spec.js
+++ server/api/test/server.spec.js
@@ -3,6 +3,7 @@ describe("server", function () {
   let api;
 
   beforeAll(async () => {
+    await initDatabase();
     api = app.listen(5000, () =>
       console.log("Test server running on port 5000")
     );
