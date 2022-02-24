31a3dece6bccc10ff6b832121796fe9b3e5713d2
jalexxx <jumaanahalex@gmail.com>
2022-02-24 12:14:19 +0000

    making last page to test animatioon

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -148,6 +148,14 @@ function renderLoginForm() {
   login_form_pages.id = "login_form_pages";
   login_form_pages.classList.add("page1","page2", "page3", "page4", "page5", "page6", "page7")
   main.appendChild(login_form_pages);
+
+  const login_last_page = document.createElement("div");
+  login_last_page.id = "login_last_page";
+  login_last_page.classList.add(last_page)
+  login_last_page.src =
+ "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
+ login_last_page.alt = "last page image";
+  main.appendChild(login_last_page);
   // const login_form_cover = document.createElement("div");
   // login_form_cover.id = "login_form_cover";
   // main.appendChild(login_form_cover);
