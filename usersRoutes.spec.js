7677508590a7e1ab5581f548b704a423113315ff
saminakhan999 <samina.khan9999@gmail.com>
2022-02-24 16:58:31 +0000

    user index test

---
+++ usersRoutes.spec.js
@@ -0,0 +1,33 @@
+describe("GET /", () => {
+  let api;
+
+  beforeAll(async () => {
+    api = app.listen(5000, () =>
+      console.log("Test server running on port 5000")
+    );
+  });
+
+  afterAll((done) => {
+    console.log("Gracefully stopping test server");
+    api.close(done);
+  });
+
+
+  test("responds with status code 403", (done) => {
+    request(api)
+    .get("/users/")
+    .expect(403, done);
+  });
+
+  test("responds with Missing Token message", (done) => {
+    request(api)
+      .get("/users/")
+      .expect({ msg: "User created" }, done);
+  });
+
+  test("responds with json", (done) => {
+    request(api)
+    .get("/users/")
+    .expect("Content-Type", /json/, done);
+  });
+});
