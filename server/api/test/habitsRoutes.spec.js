6fbe7e217a9f9434e3bf31dfd5ef6862da5ce25c
saminakhan999 <samina.khan9999@gmail.com>
2022-02-24 18:01:03 +0000

    added habit tests

---
+++ server/api/test/habitsRoutes.spec.js
@@ -0,0 +1,23 @@
+// GET
+
+
+describe("GET /:user_id", () => {
+
+  let api;
+
+  beforeAll(async () => {
+    api = app.listen(5000, () =>
+      console.log("Test server running on port 5000")
+    );
+  });
+
+  afterAll((done) => {
+    console.log("Gracefully stopping test server");
+    api.close(done);
+  });
+
+  
+
+  
+})
+
